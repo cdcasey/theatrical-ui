ed8af40240c550b812ff9fc2cd3386587a863e09
Chris Casey <cdcasey@gmail.com>
2018-07-16 09:45:22 -0500

    Placeholders for when it's time to work on drag & drop

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+
+// TODO: import not working
+
+// import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 BigCalendar.momentLocalizer(moment);
+
+// const DnDCalendar = withDragAndDrop(BigCalendar);
 
 export default class Caledar extends React.Component {
     //     componentDidMount() {
