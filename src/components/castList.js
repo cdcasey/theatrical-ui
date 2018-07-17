1d0c8345f59c0d3426b8c6f2b30f15f94d93dce4
Chris Casey <cdcasey@gmail.com>
2018-07-17 10:36:21 -0500

    Display cast info on click

--- src/components/castList.js
+++ src/components/castList.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-export default function CastList(props) {
-    return props.cast.map((castMember) => {
-        return (
-            <React.Fragment key={castMember.character}>
-                <a onClick={props.selectProduction} className="dropdown-item" href="#">
-                    {castMember.first_name} {castMember.last_name} - <strong>{castMember.character}</strong>
-                </a>
-                <div className="dropdown-divider"></div>
-            </React.Fragment>
-        )
-    })
+export default class CastList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    handleClick(event) {
+        event.preventDefault();
+        const actualTarget = event.target.tagName === 'A' ? event.target : event.target.parentElement;
+        actualTarget.nextSibling.classList.toggle('d-none');
+    }
+    render() {
+        return this.props.cast.map((castMember) => {
+            return (
+                <React.Fragment key={castMember.character}>
+                    <a onClick={this.handleClick.bind(this)} className="dropdown-item" href="#">
+                        {castMember.first_name} {castMember.last_name} - <strong>{castMember.character}</strong>
+                    </a>
+                    <div className="cast-info d-none">
+                        {castMember.email}
+                        <br />
+                        {castMember.phone}
+                    </div>
+                    <div className="dropdown-divider"></div>
+                </React.Fragment >
+            )
+        })
+    }
 }
